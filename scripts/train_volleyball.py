c8b3ec8912d980d4b0952f7e96ae9d8fa70ede1b
kruss84 <sebastiankrusche@rocketmail.com>
2023-02-04 08:03:54 +0100

    Update train_volleyball.py

--- scripts/train_volleyball.py
+++ scripts/train_volleyball.py
@@ -4,20 +4,20 @@ from train_net import *
 
 cfg=Config('volleyball')
 
-cfg.use_multi_gpu=True
+cfg.use_multi_gpu=False
 cfg.device_list="0,1"
-cfg.training_stage=2
-cfg.stage1_model_path='stage1_87%.pth'
+cfg.training_stage=1
+cfg.stage1_model_path=''
 #cfg.stage1_model_path='result/stage1_82.pth'
 #cfg.stage2_model_path='/home/junwen/opengit/ARG_group/ARG_source/result/[Volleyball_stage2_stage2]<2019-08-05_16-20-34>/stage2_epoch6_83.82%.pth'
 #cfg.resume=True
 cfg.train_backbone=False
 
-cfg.batch_size=6 
-cfg.test_batch_size=6
+cfg.batch_size=4
+cfg.test_batch_size=2
 cfg.num_frames=20
 cfg.train_learning_rate=1e-4
 # cfg.test_before_train=True
 
-cfg.exp_note='Volleyball_stage2'
+cfg.exp_note='Volleyball_stage1'
 train_net(cfg)
