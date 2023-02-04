2d7e28865afb74dfc79ce071dfb68fa81eee2a00
kruss84 <sebastiankrusche@rocketmail.com>
2023-02-04 08:19:50 +0100

    Update train_volleyball.py

--- scripts/train_volleyball.py
+++ scripts/train_volleyball.py
@@ -11,7 +11,7 @@ cfg.stage1_model_path=''
 #cfg.stage1_model_path='result/stage1_82.pth'
 #cfg.stage2_model_path='/home/junwen/opengit/ARG_group/ARG_source/result/[Volleyball_stage2_stage2]<2019-08-05_16-20-34>/stage2_epoch6_83.82%.pth'
 #cfg.resume=True
-cfg.train_backbone=False
+cfg.train_backbone=True
 
 cfg.batch_size=4
 cfg.test_batch_size=2
